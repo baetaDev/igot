aa8846a2c25edcd39dc7057e78b59d5a5de45b70
Felipe Baeta <baetafelipe2@gmail.com>
2022-11-21 00:18:17 -0300

    tenancy

---
+++ database/migrations/tenant/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->enum('status',['ativo','inativo']);
+            $table->enum('sexo',['masculino','feminino']);
+            $table->enum('tipo_de_conta',['administrador','usuario']);
+            $table->string('role')->default('ROLE_CUSTOMER');
+            $table->text('departamento')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
